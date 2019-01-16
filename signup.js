0b9d42d83f2d91f990be2d732c362722285fe3f5
Oscar Davies <oscar.william.davies@outlook.com>
2019-01-16 15:25:51 +0000

    Perform Signup

--- signup.js
+++ signup.js
@@ -5,9 +5,14 @@ function PerformLogin(){
 	UserName=document.getElementById("UsernameField").value;
 	Email=document.getElementById("EmailField").value;
 	RawPassword=document.getElementById("PasswordField").value;
-	fetch("http://localhost:4444/signup",
-		  {headers:[["UserName",UserName],["Email",Email],["Password",RawPassword]]}
-		  ,function(){
-		
+	fetch("http://localhost:775/signup",
+		  {method: 'post',
+		   headers:[["UserName",UserName],["Email",Email],["Password",RawPassword]]}).then(function(RespData){
+		RespData.json().then(function(JRespData){
+			if (JRespData["Code"]==200){
+				document.getElementById("LoginMessage").innerHTML="SignedUp";
+			}
+			else {document.getElementById("LoginMessage").innerHTML=JRespData["Message"];}
+		});
 	});
 }
