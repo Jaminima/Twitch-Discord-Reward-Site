3204d8ce0e65cfeff3bce31b48639f67ad7c38ee
Oscar Davies <oscar.william.davies@outlook.com>
2019-01-22 20:25:15 +0000

    Work on help

--- help.js
+++ help.js
@@ -2,5 +2,27 @@
 var Currency;
 LoadHelp();
 function LoadHelp(){
-	Currency = GetCurrency(1);
+	GetCurrency(1).then(
+		function(CurrencyData){
+			Currency=CurrencyData;
+			WriteHelp();
+		}
+	);
+}
+
+function WriteHelp(){
+	Currency["CommandConfig"]["CommandSetup"]["Balance"]["Commands"].forEach(
+		function(Text){
+			var Element=document.getElementById("BalanceCommands");
+			if (Element.innerHTML!=""){ Element.innerHTML+=", "; }
+			Element.innerHTML+=Text;
+		}
+	);
+	Currency["CommandConfig"]["CommandSetup"]["Pay"]["Commands"].forEach(
+		function(Text){
+			var Element=document.getElementById("PayCommands");
+			if (Element.innerHTML!=""){ Element.innerHTML+=", "; }
+			Element.innerHTML+=Text;
+		}
+	);
 }
