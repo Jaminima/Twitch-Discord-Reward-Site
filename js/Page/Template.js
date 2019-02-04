155b1fdc3ad05f03343a5f5318f39dd0f9fc0fb8
Oscar Davies <oscar.william.davies@outlook.com>
2019-02-04 07:35:51 +0000

    Help Pages

---
+++ js/Page/Template.js
@@ -0,0 +1,105 @@
+// JavaScript Document
+var LoginLogout=document.getElementById("Login/Logout"),
+	SignUpProfile=document.getElementById("SignUp/Profile"),
+	OverlayButton=document.getElementById("OverlayButton"),
+	Overlay=document.getElementById("Overlay"),
+	OverlayMessage=document.getElementById("OverlayMessage");
+SetProfileBar();
+
+function SetProfileBar(){
+	GetCurrentLogin(DataFromCookies()["Token"]).then(
+		function(LoginData){
+			if (LoginData!=null){
+				LoginLogout.innerHTML="Logout";
+				LoginLogout.onclick=PerformLogout;
+				SignUpProfile.innerHTML="My Profile";
+				SignUpProfile.onclick=GotoProfile;
+			}
+			else {
+				LoginLogout.innerHTML="Login";
+				LoginLogout.onclick=DrawSignIn;
+				SignUpProfile.innerHTML="SignUp";
+				SignUpProfile.onclick=DrawSignUp;
+			}
+		}
+	);
+}
+
+function DataFromCookies(){
+	var Dictionary = {};
+	var Cookies=document.cookie.split(";");
+	for (var i=0;i<Cookies.length;i++){
+		var Cookie=Cookies[i].split("=");
+		Dictionary[Cookie[0].replace(" ","")]=Cookie[1];
+	}
+	return Dictionary;
+}
+
+function HideOverlay(){
+	document.getElementById("OverlayUsernameField").value="";
+	document.getElementById("OverlayEmailField").value="";
+	document.getElementById("OverlayPasswordField").value="";
+	document.getElementById("Overlay").style.display="none";
+}
+
+function PanelClicked(activity){
+	var event = activity ? activity : window.event;
+    if (event.stopPropagation) {event.stopPropagation();}
+    else {event.cancelBubble=true;}
+    return false;
+}
+
+function DrawSignUp(){
+	Overlay.style.display="block";
+	OverlayButton.onclick=PerformSignUp;
+	OverlayButton.value="Sign Up";
+}
+function DrawSignIn(){
+	Overlay.style.display="block";
+	OverlayButton.onclick=PerformSignIn;
+	OverlayButton.value="Sign In";
+}
+
+function PerformSignUp(){
+	var UserName=document.getElementById("OverlayUsernameField").value,
+		Email=document.getElementById("OverlayEmailField").value,
+		RawPassword=document.getElementById("OverlayPasswordField").value;
+	SignUp(UserName,Email,RawPassword).then(
+		function(SignUpData){
+			if (SignUpData["Code"]==200){
+				PerformSignIn();
+			}
+			else { OverlayMessage.innerHTML=SignUpData["Message"].replace("Bad Request, ","").replace("header",""); }
+		}
+	);
+}
+
+function PerformSignIn(){
+	var UserName=document.getElementById("OverlayUsernameField").value,
+		Email=document.getElementById("OverlayEmailField").value,
+		RawPassword=document.getElementById("OverlayPasswordField").value;
+	Login(UserName,Email,RawPassword).then(
+		function(SignInData){
+			if (SignInData["Code"]==200){
+				document.cookie="Token="+SignInData["Data"]["AccessToken"]+";"
+				document.cookie="ID="+SignInData["Data"]["ID"];
+				SetProfileBar();
+				HideOverlay();
+			}
+			else { OverlayMessage.innerHTML=SignInData["Message"].replace("Bad Request, ","").replace("header",""); }
+		}
+	);
+}
+
+function PerformLogout(){
+	if (window.confirm("Are you sure you want to sign out?")) {
+		document.cookie="ID=;";
+		document.cookie="Token=;"
+		document.location.href=".";
+	} 
+	else { return; }
+}
+
+function GotoProfile(){
+
+}
--- js/login.js
+++ js/login.js
@@ -1,58 +1,46 @@
 // JavaScript Document
-var UserName,Email,RawPassword;
 
-CheckLoggedin();
-
-function Signin(){
-	UserName=document.getElementById("UsernameField").value;
-	Email=document.getElementById("EmailField").value;
-	RawPassword=document.getElementById("PasswordField").value;
+function Login(UserName,Email,RawPassword){
 	var Headers=[["Password",RawPassword]];
 	if (UserName!="") {Headers.push(["UserName",UserName]);}
 	if (Email!="") {Headers.push(["Email",Email]);}
-	fetch("https://owlcoin.co.uk/webapi/login",
-		  {method: 'post',
-		   headers:Headers}).then(function(RespData){
-		RespData.json().then(function(JRespData){
-			if (JRespData["Code"]==200){
-				document.getElementById("LoginMessage").innerHTML="SignedIn";
-				document.cookie=JRespData["Data"]["AccessToken"];
-				document.location.href="./profile.html";
-			}
-			else {document.getElementById("LoginMessage").innerHTML=JRespData["Message"].replace("Bad Request, ","");}
-		});
+	return fetch("https://owlcoin.co.uk/webapi/login", {
+		method: 'post',
+		headers: Headers
+	}).then(function (RespData) {
+		return RespData.json();
+	}).then(function (JRespData) {
+		return JRespData;
 	});
 }
 
-function Signup(){
-	UserName=document.getElementById("UsernameField").value;
-	Email=document.getElementById("EmailField").value;
-	RawPassword=document.getElementById("PasswordField").value;
+function SignUp(UserName,Email,RawPassword){
 	var Headers=[["Password",RawPassword]];
 	if (UserName!="") {Headers.push(["UserName",UserName]);}
 	if (Email!="") {Headers.push(["Email",Email]);}
-	fetch("https://owlcoin.co.uk/webapi/signup",
-		  {method: 'post',
-		   headers:Headers}).then(function(RespData){
-		RespData.json().then(function(JRespData){
-			if (JRespData["Code"]==200){
-				document.getElementById("LoginMessage").innerHTML="SignedUp";
-				Signin();
-			}
-			else {document.getElementById("LoginMessage").innerHTML=JRespData["Message"].replace("Bad Request, ","");}
-		});
+	return fetch("https://owlcoin.co.uk/webapi/signup", {
+		method: 'post',
+		headers: Headers
+	}).then(function (RespData) {
+		return RespData.json();
+	}).then(function (JRespData) {
+		return JRespData;
 	});
 }
 
-function CheckLoggedin(){
-	fetch("https://owlcoin.co.uk/webapi/login",
-		  {method: 'get',
-		   headers:[["AccessToken",document.cookie]]}).then(function(RespData){
-		RespData.json().then(function(JRespData){
-			if (JRespData["Code"]==200){
-				document.location.href="./profile.html";
-			}
-			else { }
-		});
+function GetCurrentLogin(AccessToken){
+	return fetch("https://owlcoin.co.uk/webapi/login", {
+		method: 'get',
+		headers: [
+			["AccessToken",AccessToken]
+		]
+	}).then(function (RespData) {
+		return RespData.json();
+	}).then(function (JRespData) {
+		if (JRespData["Code"] == 200) {
+			return JRespData["Data"];
+		} else {
+			return null;;
+		}
 	});
 }
